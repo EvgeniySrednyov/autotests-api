b94627ca5e481e6c149eadd68a13fcf214c68bb7
EvgeniySrednev <srednevei89@gmail.com>
2025-06-10 14:37:34 +0700

    8.7 Параметризация Pytest. Практика работы с pytest параметризацией

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,14 +1,19 @@
 from http import HTTPStatus
 import pytest
+from clients.users.private_users_client import PrivateUsersClient
 from clients.users.public_users_client import PublicUsersClient
 from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema, GetUserResponseSchema
+from fixtures.users import UserFixture
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
 from tools.assertions.users import assert_create_user_response, assert_get_user_response
+from tools.fakers import fake
 
 
-def test_create_user(public_users_client: PublicUsersClient):
-    request = CreateUserRequestSchema()
+@pytest.mark.parametrize('email', ['mail.ru', 'gmail.com', 'example.com'])
+def test_create_user(email: str, public_users_client: PublicUsersClient):
+    user_email = fake.email(email)
+    request = CreateUserRequestSchema(email=user_email)
     response = public_users_client.create_user_api(request)
     response_data = CreateUserResponseSchema.model_validate_json(response.text)
 
@@ -20,7 +25,7 @@ def test_create_user(public_users_client: PublicUsersClient):
 
 @pytest.mark.regression
 @pytest.mark.users
-def test_get_user_me(private_users_client, function_user):
+def test_get_user_me(private_users_client: PrivateUsersClient, function_user: UserFixture):
     response = private_users_client.get_users_me_api()
     response_data = GetUserResponseSchema.model_validate_json(response.text)
 
@@ -28,3 +33,5 @@ def test_get_user_me(private_users_client, function_user):
     assert_get_user_response(response_data, function_user.response)
 
     validate_json_schema(response.json(), response_data.model_json_schema())
+
+
