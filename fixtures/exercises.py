69524a5543408d4f0b1958e38717917eca41532f
EvgeniySrednev <srednevei89@gmail.com>
2025-06-26 13:20:30 +0700

    9.4 Практикуемся в написании API автотестов. Часть 3. Практика написания API автотестов для работы с заданиями. Получение списка заданий

--- fixtures/exercises.py
+++ fixtures/exercises.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel
 from clients.courses.courses_client import CoursesClient
 from clients.exercises.exercises_client import get_exercises_client, ExercisesClient
 from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema
+from fixtures.courses import CourseFixture
 from fixtures.users import UserFixture
 
 
@@ -21,8 +22,8 @@ def exercises_client(function_user: UserFixture) -> ExercisesClient:
 def function_exercise(
         exercises_client: ExercisesClient,
         function_user: UserFixture,
-        function_course: CoursesClient
+        function_course: CourseFixture
 ) -> ExerciseFixture:
-    request = CreateExerciseRequestSchema()
+    request = CreateExerciseRequestSchema(course_id=function_course.response.course.id)
     response = exercises_client.create_exercise(request)
     return ExerciseFixture(request=request, response=response)
